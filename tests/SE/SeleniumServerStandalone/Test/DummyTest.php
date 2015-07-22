ed4c113c3d3a929739b857669db28c4e3f3447e7
Sven Eisenschmidt <s.eisenschmidt@impericon.com>
2015-07-22 08:07:39 +0200

    PHPUnit support

---
+++ tests/SE/SeleniumServerStandalone/Test/DummyTest.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * This file is part of sveneisenschmidt/selenium-server-standalone
+ *
+ * (c) Sven Eisenschmidt <sven.eisenschmidt@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace SE\SeleniumServerStandalone\Test;
+
+class DummyTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     *
+     * @test
+     */
+    public function does_run_correctly()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * This file is part of sveneisenschmidt/selenium-server-standalone
+ *
+ * (c) Sven Eisenschmidt <sven.eisenschmidt@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+require __DIR__ . '/../vendor/autoload.php';
